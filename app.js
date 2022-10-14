de3fc060b580b24984ab51a98b6e39d3fea68dc7
tatang <tatang.romadhona@rushowl.sg>
2022-10-14 18:36:32 +0700

    inic commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        status: true,
+        message: 'deploy ke vercel'
+    });
+});
+
+app.listen(3000, () => console.log('listening on port', PORT));
