d0909ce9de9e7589b6c7fd3078e6794a66992999
tatang <tatang.romadhona@rushowl.sg>
2022-10-14 18:46:05 +0700

    update index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res, next) => {
     return res.status(200).json({
         status: true,
